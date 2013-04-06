c37373726e6a06b481cec3bb26006e262b92fb1a
Andres J Ruiz Torres <andresruiz2010@gmail.com>
2013-04-06 13:22:55 -0400

    Make the WebView usable, starting some work on getting NFC

--- project/src/com/panstora/MainActivity.java
+++ project/src/com/panstora/MainActivity.java
@@ -2,24 +2,35 @@ package com.panstora;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.provider.Settings.Secure;
 
 public class MainActivity extends Activity {
 
+	WebView myWebView;
+
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		WebView myWebView = (WebView) findViewById(R.id.webView1);
+		myWebView = (WebView) findViewById(R.id.webView1);
 		WebSettings webSettings = myWebView.getSettings();
 		webSettings.setJavaScriptEnabled(true);
-		myWebView.setWebViewClient(new WebViewClient());
+		myWebView.setWebViewClient(new WebViewClient()); 
 		myWebView.loadUrl("http://andresjruiz.com");
 		
+		
+		String android_id = Secure.getString(getBaseContext().getContentResolver(),
+                Secure.ANDROID_ID);
+		
+		Log.d("MainActivity", "Private android id = " + android_id);
 	}
 
 	@Override
@@ -27,5 +38,21 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		//getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		
+		//Force the back key to work
+		
+	    // Check if the key event was the Back button and if there's history
+	    if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+	        myWebView.goBack();
+	        return true;
+	    }
+	    // If it wasn't the Back key or there's no web page history, bubble up to the default
+	    // system behavior (probably exit the activity)
+	    return super.onKeyDown(keyCode, event);
 	}
 }
